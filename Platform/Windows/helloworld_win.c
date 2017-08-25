1881770f64890e86f873408d07641d418d1d2c0f
cyc <149779511@qq.com>
2017-08-25 09:06:25 +0800

    hellow_win.c

---
+++ Platform/Windows/helloworld_win.c
@@ -0,0 +1,17 @@
+#include <windows.h>
+#include <windowsx.h>
+#include <tchar.h>
+// the WindowProc function prototype
+LRESULT CALLBACK WindowProc(HWND hWnd,
+        UINT message,
+        WPARAM wParam,
+        LPARAM lParam);
+
+// the entry point for any Windows program
+int WINAPI WinMain(HINSTANCE hInstance,
+        HINSTANCE hPrevInstance,
+        LPTSTR lpCmdLine,
+        int nCmdShow)
+{
+
+}
