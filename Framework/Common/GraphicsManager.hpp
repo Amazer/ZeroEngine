f6cb45f3da6a070db1054d741fa671a76be8b5df
cyc <149779511@qq.com>
2017-08-25 22:32:02 +0800

    article 7,code

---
+++ Framework/Common/GraphicsManager.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include "IRunTimeModule.hpp"
+namespace ZE
+{
+    class GraphicsManager : implements IRuntimeModele
+    {
+        public:
+            virtual ~ GraphicsManager(){}
+    };
+}
---
+++ build_cmake.bat
@@ -0,0 +1,4 @@
+cd build_cmake
+cmake -G "Unix Makefiles" ..
+make
+pause
